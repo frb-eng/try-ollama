9134815506ec3c8746a71171515f5beda986cf4a
Igor Bari <mucsi96@gmail.com>
2024-09-07 14:06:38 +0200

    improve env setup

---
+++ create_env.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+conda create --prefix .conda --yes python=3.12
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-brew update && brew upgrade && brew install miniconda
-
-if [ ! -d ".conda" ]; then
-    conda init
-    conda create --prefix .conda --yes python=3.12
-fi
-
-conda activate $PWD/.conda
 pip install -r requirements.txt
